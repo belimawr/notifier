49edb261a13033b1320e5b46bb23067ead1eb21b
Tiago Queiroz <contato@tiago.eti.br>
2018-11-22 23:55:17 +0100

    parser: adding version
    This commits populates the `Version` filed of the message returned by the git implementation of `Parser`

--- parser/git.go
+++ parser/git.go
@@ -14,7 +14,7 @@ var (
 	eol = "<<EOL>>\n"
 
 	// fields is a list of fields to be extracted from git log
-	fields = []string{"%an", "%s", "%b"}
+	fields = []string{"%an", "%s", "%b", "%h"}
 )
 
 type git struct{}
@@ -22,22 +22,30 @@ type git struct{}
 func (g git) ParseMessage() (resources.Message, error) {
 	format := fmt.Sprintf("--format=%s", strings.Join(fields, eol))
 
-	rawOutput, err := exec.Command("git", "log", "-n1", format).Output()
+	rawGitLog, err := exec.Command("git", "log", "-n1", format).Output()
 	if err != nil {
 		return resources.Message{}, err
 	}
 
-	output := string(rawOutput)
+	gitLog := string(rawGitLog)
 
-	split := strings.Split(output, eol)
+	split := strings.Split(gitLog, eol)
 	for i := 0; i < len(split); i++ {
 		split[i] = strings.TrimSpace(split[i])
 	}
+
+	rawVersion, err := exec.Command("git", "describe", "--tags", split[3]).Output()
+	if err != nil {
+		return resources.Message{}, err
+	}
+
+	ver := strings.TrimSpace(string(rawVersion))
 
 	msg := resources.Message{
 		Author:      split[0],
 		Title:       split[1],
 		Description: split[2],
+		Version:     ver,
 	}
 
 	return msg, nil
