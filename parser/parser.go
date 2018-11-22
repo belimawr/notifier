87143840f0ac137e17f0b91f5fb7deda697b41a5
Tiago Queiroz <contato@tiago.eti.br>
2018-11-22 23:38:48 +0100

    parser package
    This commit adds the `parser` package that is responsible for reading and parsing messages.
    
    This commit contains only the definition of the interface

---
+++ parser/parser.go
@@ -0,0 +1,15 @@
+// Package parser is responsible for reading and parsing the messages
+// currently it contains only a implementation to parse messages from
+// git
+package parser
+
+import "github.com/belimawr/notifier/resources"
+
+// Parser abstracts the reading and parsing of messages
+// TODO: decide if reading of a message (e.g.: getting the output of
+// `git log`) should be in a separeted interface/method
+type Parser interface {
+	// ParseMessage returns a parsed message ready to be sent on any
+	// notification channel
+	ParseMessage() (resources.Message, error)
+}
